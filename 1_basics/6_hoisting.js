015fd32244c6d14638d90eb82284f36de26ecb19
hgkimdev <hgkimdev@proton.me>
2026-01-22 00:05:55 +0900

    01-21

---
+++ 1_basics/6_hoisting.js
@@ -0,0 +1,23 @@
+/**
+ * Hoisting
+ */
+console.log('hello');
+console.log('world');
+console.log('----------------');
+
+// console.log(name);
+// var name = 'jane doe';
+// console.log(name);
+
+// var name;
+// console.log(name);
+// name = 'john doe';
+// console.log(name);
+
+/**
+ * Hoisting은
+ * 모든 변수 선언이 최상단으로 끌어올려지는 것처럼 동작하는 자바스크립트의 특성
+ */
+
+console.log(yuJin);
+let yuJin = '안유진';
