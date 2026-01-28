9781ab1d4b4b64401b9fa0469dfba83a475c4ec2
hgkimdev <hgkimdev@proton.me>
2026-01-28 19:59:05 +0900

    01-28

---
+++ 2_class_and_oop/1_class_keyword.js
@@ -0,0 +1,44 @@
+/**
+ * Class Keyword
+ */
+
+class IdolModel{
+  name;
+  year;
+
+  constructor(name, year){
+    this.name = name;
+    this.year = year;
+  }
+
+  sayName(){
+    return `안녕하세요 저는 ${this.name}입니다.`
+  }
+}
+
+const yuJin = new IdolModel('안유진', 2003);
+console.log(yuJin);
+const gaeul = new IdolModel('가을', 2002);
+console.log(gaeul);
+const ray = new IdolModel('레이', 2004);
+console.log(ray);
+const wonYoung = new IdolModel('장원영', 2004);
+console.log(wonYoung);
+const liz = new IdolModel('리즈', 2004);
+console.log(liz);
+const eseo = new IdolModel('이서', 2007);
+console.log(eseo);
+
+console.log(yuJin.name);
+console.log(yuJin.year);
+
+console.log(yuJin.sayName());
+console.log(wonYoung.sayName());
+
+console.log(typeof IdolModel);
+//js에서 class는 function으로 인식됨
+console.log(typeof yuJin);
+//class로 생성한 object는 instance
+
+
+// constructor - 생성자
