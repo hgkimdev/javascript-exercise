677e66d960ae4806e1c8b3709964365df89bf670
hgkimdev <hgkimdev@proton.me>
2026-01-31 23:55:12 +0900

    01-31

--- 3_all_about_object/3_immutable_object.js
+++ 3_all_about_object/3_immutable_object.js
@@ -69,3 +69,49 @@ Object.defineProperty(yuJin2, 'name', {
 console.log(Object.getOwnPropertyDescriptor(yuJin2, 'name'));
 
 // seal = configurable: false
+
+/**
+ * Freezed
+ * 
+ * 읽기 외에 모든 기능을 불가능하게 만든다.
+ */
+
+const yuJin3 = {
+  name: '안유진',
+  year: 2003,
+
+  get age(){
+    return new Date().getFullYear() - this.year;
+  },
+
+  set age(age){
+    this.year = new Date().getFullYear() - age;
+  }
+}
+console.log(Object.isFrozen(yuJin3));
+
+Object.freeze(yuJin3);
+console.log(Object.isFrozen(yuJin3));
+
+yuJin3['groupName'] = '아이브';
+console.log(yuJin3);
+
+delete yuJin3['name']
+console.log(yuJin3);
+
+// Object.defineProperty(yuJin3, 'name', {
+//   value: '코드팩토리',
+// })
+console.log(Object.getOwnPropertyDescriptor(yuJin3,'name'));
+
+const yuJin4 = {
+  name: '안유진',
+  year: 2003,
+  wonYong: {
+    name: '장원영',
+    year: 2002,
+  },
+};
+Object.freeze(yuJin4);
+console.log(Object.isFrozen(yuJin4));
+console.log(Object.isFrozen(yuJin4['wonYoung']));
---
+++ 3_all_about_object/4_using_function_to_object.js
@@ -0,0 +1,17 @@
+/**
+ * Using function to create object
+ */
+function IdolModel(name, year){
+  this.name = name;
+  this.year = year;
+
+  this.dance = function(){
+    return `${this.name}이 춤을 춥니다.`;
+  }
+}
+const yuJin = new IdolModel('안유진', 2003);
+console.log(yuJin);
+console.log(yuJin.dance());
+const yuJin2 = IdolModel('안유진', 2003);
+console.log(yuJin2);
+console.log(global.name); //쓰지마
