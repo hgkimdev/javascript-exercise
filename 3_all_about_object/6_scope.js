48da945bca824de95b613cac056d3d78e92f9dd4
hgkimdev <hgkimdev@proton.me>
2026-02-09 23:59:22 +0900

    02-09

---
+++ 3_all_about_object/6_scope.js
@@ -0,0 +1,20 @@
+/**
+ * Scope
+ */
+var numberOne = 20;
+
+function levelOne() {
+  console.log(numberOne);
+}
+
+levelOne();
+
+function levelOne() {
+    var numberOne = 40;
+
+    console.log(numberOne);
+};
+
+levelOne();
+
+console.log(numberOne);
